c4317a16d81fb8c25a0344f5f23f0da1981bb97a
cg1101 <cg1101@gmail.com>
2016-05-13 11:57:29 +1000

    added one basic test.

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python
+
+import unittest
+
+from flask import url_for
+
+class MyTestCase(unittest.TestCase):
+
+	def setUp(self):
+		self.app = app = create_app('testing')
+		self.client = app.test_client()
+
+	def tearDown(self):
+		pass
+
+	def test_whoami(self):
+		testcase = self
+		url = url_for('whoami')
+		rv = self.client.open(url, method='GET')
+		testcase.assertInInstance(rv, dict)
+		testcase.assertTrue('message' in rv)
+		testcase.assertIsInstance(rv['message'], basestring)
